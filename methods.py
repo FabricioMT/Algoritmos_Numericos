38d8fdf081ad8a37739659d59d80337c0bdd01ce
FabricioMT <fabricio.medeiros3@hotmail.com>
2022-07-08 17:17:34 -0300

    att

--- methods.py
+++ methods.py
@@ -1,7 +1,44 @@
+import sys
 import numpy as np
-from readArqs import Args
 from timeit import default_timer as timer
 from datetime import timedelta
+from os import listdir
+
+def readFile(inputs):
+    shape = tuple(np.loadtxt(fname=inputs, dtype=int, delimiter=',', max_rows=1, usecols=(0,1)))
+    PreSim = np.loadtxt(fname=inputs, dtype=str, delimiter=',', max_rows=1, usecols=(2,3))
+    A = np.loadtxt(fname=inputs, dtype=np.float64, delimiter=',', skiprows=1, max_rows=shape[1], usecols=np.arange(0,shape[1]))
+    B = np.loadtxt(fname=inputs, dtype=np.float64, delimiter=',', skiprows=shape[1], max_rows=shape[0], usecols=np.arange(0,shape[1]))
+
+    if shape[0] != 1:
+        B = np.reshape(B, (shape[0],shape[1],1))
+    else:
+        B = np.reshape(B, (shape[1],shape[0]))
+
+    A = np.reshape(A, (shape[1],shape[1]))
+    P = np.float64(PreSim[0])
+    S = PreSim[1]
+
+    return A, B, P, S
+
+def readArgs():
+    if len(sys.argv) > 2:
+        arq = 0
+        file_input = listdir('./inputs/')
+        for file in file_input:
+            if file == sys.argv[1]:
+                arq = 'inputs/' + sys.argv[1]
+        
+        args = sys.argv[1:]
+        print(f"Arguments count: {len(sys.argv)}")
+        print(f"Arguments of the script : {args}")
+        if arq == 0:
+            print("Arquivo não encontrado nos Inputs ou Arquivo com nome Inválido !")
+            exit(1) 
+    else:
+        print("Entrada de Dados Inválida !")
+        exit(1)
+    return arq, sys.argv[2]
 
 def gauss(A, B):
     start = timer()
@@ -36,10 +73,14 @@ def fatoraLU(A):
     print(f"\ntempo de execução LU: {timing}\n")
     return L, U
 
+def saveOutput(output, X):
+	np.savetxt(output, X, delimiter=',', header='Resposta')
 
 if __name__ == '__main__':
-    A, B, precision, simet = Args()
-
+    inputs, outputs = readArgs()
+    A, B, precision, simet = readFile(inputs)
+    saveOutput(outputs, A)
+    
     """
     print(gauss(A, B, dimensionM))
 
